d42714a7940eab9939e623e31a44ceda0305332a
Himanshuvs <goolushivde23@gmai.com>
2025-02-23 18:33:03 +0530

    commit

--- IOC Annotaion driven congif/src/com/nt/bean/WeekDayFinder.java
+++ IOC Annotaion driven congif/src/com/nt/bean/WeekDayFinder.java
@@ -1,4 +1,25 @@
 package com.nt.bean;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+@Component("wdf")
 public class WeekDayFinder {
+    private LocalDate  date;
+
+    @Autowired
+    public WeekDayFinder(LocalDate date) {
+        this.date=date;
+    }
+    public String ShowWeekDay()  {
+
+         int dayOfWeek=date.getDayOfWeek().getValue();
+         String Day=date.getDayOfWeek().toString();
+
+        if(dayOfWeek>=5)
+             return "Today is "+Day+" So Weekend ";
+         else
+             return "Today  is "+Day+" So Weekday ";
+    }
 }
--- IOC Annotaion driven congif/src/com/nt/main/dependencyInjection.java
+++ IOC Annotaion driven congif/src/com/nt/main/dependencyInjection.java
@@ -1,4 +1,13 @@
 package com.nt.main;
 
+import com.nt.bean.WeekDayFinder;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
 public class dependencyInjection {
+    public static void main(String[] args) {
+        FileSystemXmlApplicationContext context = new FileSystemXmlApplicationContext("src/com/nt/cfgs/applicationContext.xml");
+        WeekDayFinder WeekFind = context.getBean("wdf", WeekDayFinder.class);
+        String result = WeekFind.ShowWeekDay();
+        System.out.println(result);
+    }
 }
